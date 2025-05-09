126a08e88973cf5595914821ec6c3683bd3c2df4
Eranmonnie <ajalaferanmi123@gmail.com>
2025-05-09 14:52:39 +0100

    redis port

--- api/server.go
+++ api/server.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"fmt"
+	"github/Eranmonnie/jqueue/config"
 	"github/Eranmonnie/jqueue/database"
 	"log"
 	"net/http"
@@ -8,26 +10,34 @@ import (
 )
 
 type ServerConfig struct {
-	Port string
-	Db   *database.DatabaseConfig
+	Port      string
+	RedisPort string
+	Db        *database.DatabaseConfig
 }
 
-func StartServer(config ServerConfig) error {
+func StartServer(sConfig ServerConfig) error {
 	//post to create a job
 	// and get a job
 	//and probably filters
 
 	s := &http.Server{
-		Addr:           config.Port,
+		Addr:           sConfig.Port,
 		Handler:        nil,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	if config.Db != nil {
+	if sConfig.RedisPort != "" {
+		log.Println("Redis configuration provided, initializing Redis connection...")
+		config.InitRedis(sConfig.RedisPort)
+	} else {
+		return fmt.Errorf("no Redis configuration provided important for queue")
+	}
+
+	if sConfig.Db != nil {
 		log.Println("Database configuration provided, initializing database connection...")
-		database, err := database.NewGormDB(*config.Db)
+		database, err := database.NewGormDB(*sConfig.Db)
 		if err != nil {
 			log.Fatalf("Failed to create database connection: %v", err)
 		}
@@ -43,6 +53,6 @@ func StartServer(config ServerConfig) error {
 		log.Println("No database configuration provided")
 	}
 
-	log.Println("Server starting on port", config.Port)
+	log.Println("Server starting on port", sConfig.Port)
 	return s.ListenAndServe()
 }
